a373d07d18d313c0b293d8c29e011e7c4798171e
xioperez01 <angie.xioperez@gmail.com>
2021-07-27 17:38:33 -0500

    Task 10 (Advanced)

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,10 @@
-
 #!/usr/bin/env python3
 """python module"""
 from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    '''any and none'''
+    """return any or none"""
     if lst:
         return lst[0]
     else:
