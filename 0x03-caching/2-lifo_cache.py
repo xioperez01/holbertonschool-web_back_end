0218b65cfd6021b68a40f25a9c61b471e65dac74
xioperez01 <angie.xioperez@gmail.com>
2021-07-26 20:20:58 -0500

    Initial code

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""python module"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFOCache class"""
+
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if self.queue:
+                last = self.queue.pop()
+                del self.cache_data[last]
+                print("DISCARD: {}".format(last))
+
+        if key not in self.queue:
+            self.queue.append(key)
+        else:
+            self.mv_last_list(key)
+
+    def get(self, key):
+        return self.cache_data.get(key, None)
+
+    def mv_last_list(self, item):
+        length = len(self.queue)
+        if self.queue[length - 1] != item:
+            self.queue.remove(item)
+            self.queue.append(item)
---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""python module"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRUChache class"""
+
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first = self.get_first_list(self.queue)
+            if first:
+                self.queue.pop(0)
+                del self.cache_data[first]
+                print("DISCARD: {}".format(first))
+
+        if key not in self.queue:
+            self.queue.append(key)
+        else:
+            self.mv_last_list(key)
+
+    def get(self, key):
+        item = self.cache_data.get(key, None)
+        if item is not None:
+            self.mv_last_list(key)
+        return item
+
+    def mv_last_list(self, item):
+        length = len(self.queue)
+        if self.queue[length - 1] != item:
+            self.queue.remove(item)
+            self.queue.append(item)
+
+    @staticmethod
+    def get_first_list(array):
+        return array[0] if array else None
