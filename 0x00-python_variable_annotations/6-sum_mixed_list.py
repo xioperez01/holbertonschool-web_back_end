f581d2a056f073557738e7d03b3a5e2bbfd40c01
xioperez01 <angie.xioperez@gmail.com>
2021-07-26 21:02:10 -0500

    Initial code task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""python module"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Return a float sum"""
+    return(sum(mxd_lst))
