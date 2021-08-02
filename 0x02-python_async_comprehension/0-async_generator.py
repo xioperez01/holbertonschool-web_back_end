bb0f649b6f28cb4935bde8f27089b71fab2419e4
xioperez01 <angie.xioperez@gmail.com>
2021-08-02 10:07:53 -0500

    Initial code task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Python Module"""
+
+import random
+import asyncio
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    '''coroutine'''
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
