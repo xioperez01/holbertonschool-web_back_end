20d8a5a0e85e52f35e387eaeaf34879aebef27ab
xioperez01 <angie.xioperez@gmail.com>
2021-08-02 10:18:57 -0500

    Initial code task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+""" Python module """
+from asyncio import gather
+from time import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ """
+
+    start = time()
+    tasks = [async_comprehension() for _ in range(4)]
+    await gather(*tasks)
+    end = time()
+    return end - start
