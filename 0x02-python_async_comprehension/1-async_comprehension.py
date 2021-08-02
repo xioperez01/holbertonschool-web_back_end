d4248aa7db9b9ca47f755810b63cdd8126cc2041
xioperez01 <angie.xioperez@gmail.com>
2021-08-02 10:13:52 -0500

    fixing code task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """ Python Module """
 
-import random
-import asyncio
 from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """ Async """
     nums = [i async for i in async_generator()]
     return nums
