148f84cbffafba790ec3613352d620cebae3766d
xioperez01 <angie.xioperez@gmail.com>
2021-08-05 15:58:25 -0500

    Pep 8

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -33,6 +33,7 @@ def unauthorized() -> str:
     """
     abort(401)
 
+
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """ GET /api/v1/forbidden
