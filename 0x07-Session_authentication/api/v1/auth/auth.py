5b6221ad59d3fd14d2d00b27bc177e6b037f5300
xioperez01 <angie.xioperez@gmail.com>
2021-08-11 22:07:50 -0500

    auth

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth():
@@ -13,11 +14,11 @@ class Auth():
         if path is None or (excluded_paths is None or
                             len(excluded_paths) == 0):
             return True
-        if path[-1:] != '/':
-            path += '/'
-        if path not in excluded_paths:
-            return True
-        return False
+        if path[-1] != '/':
+            path = path + '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Returns none """
@@ -30,3 +31,11 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """ returns None """
         return None
+
+    def session_cookie(self, request=None):
+        """returns a cookie """
+        if request is None:
+            return None
+        name_var = os.getenv('SESSION_NAME')
+        value = request.cookies.get(name_var)
+        return value
