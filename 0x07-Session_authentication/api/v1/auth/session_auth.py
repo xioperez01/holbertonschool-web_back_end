1ebfca9736aba09506772628a53528c81f6f39d8
xioperez01 <angie.xioperez@gmail.com>
2021-08-11 22:02:28 -0500

    Sesion auth

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+""" Python Module """
+import uuid
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """ inherits from Auth """
+
+    user_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id
+        """
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        SessionAuth.user_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ user_id_for_session_id function """
+        if session_id is None:
+            return None
+        if type(session_id) is None:
+            return None
+        return SessionAuth.user_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ returns a User instance """
+        cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(cookie)
+        return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """ logout """
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        if not user_id:
+            return False
+
+        try:
+            del self.user_by_session_id[session_id]
+        except Exception:
+            pass
+
+        return True
