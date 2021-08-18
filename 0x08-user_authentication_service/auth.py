bc762e45dcda318f31f00a795994853ee5db66f1
xioperez01 <angie.xioperez@gmail.com>
2021-08-18 14:57:02 -0500

    Task 5: Register user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -10,3 +10,19 @@ def _hash_password(password: str) -> str:
     Returns salted, hashed pswd as bytestring
     """
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """take an email and password and return a User object."""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except Exception:
+            return self._db.add_user(email, _hash_password(password))
