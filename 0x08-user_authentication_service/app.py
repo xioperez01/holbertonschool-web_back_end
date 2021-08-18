05048b8cb34d3320380a07d6da6e3963eee0c798
xioperez01 <angie.xioperez@gmail.com>
2021-08-18 15:39:32 -0500

    Task 7: Register user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -17,5 +17,19 @@ def welcome() -> str:
     return jsonify(msg)
 
 
+@app.route('/users', methods=['POST'])
+def users():
+    """Register users"""
+    req_par = request.form
+    email = req_par.get('email')
+    password = req_par.get('password')
+    try:
+        new_user = AUTH.register_user(email, password)
+        msg = {"email": new_user.email, "message": "user created"}
+        return jsonify(msg)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
