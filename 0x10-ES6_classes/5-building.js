a3ff4074e757b200eab2fe3a77f07f3bebade273
xioperez01 <angie.xioperez@gmail.com>
2021-09-18 13:24:57 -0500

    Fix style

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,14 +1,8 @@
 class Building {
   constructor(sqft) {
-    if (
-      this.constructor !== Building
-      && typeof this.evacuationWarningMessage !== 'function'
-    ) {
-      throw Error(
-        'Class extending Building must override WarningMessage',
-      );
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-
     this._sqft = sqft;
   }
 
