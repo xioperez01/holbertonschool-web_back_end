4323070dc3367486e7430ae3a9cce1960300b140
xioperez01 <angie.xioperez@gmail.com>
2021-09-18 12:51:03 -0500

    Fix task 6

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,29 +1,33 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName,
-) {
-  const user = {};
-  const photo = {};
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = {
+    status: 'pending ',
+  };
+  const promise2 = {
+    status: 'pending ',
+  };
 
   try {
-    const signUp = await signUpUser(firstName, lastName);
-    user.status = 'fulfilled';
-    user.value = signUp;
-  } catch (error) {
-    user.status = 'rejected';
-    user.value = error.toString();
+    const signup = await signUpUser(firstName, lastName);
+    promise1.status = 'fulfilled';
+    promise1.value = signup;
+  } catch (err) {
+    promise1.status = 'rejected';
+    promise1.value = err.toString();
   }
+
   try {
     const upload = await uploadPhoto(fileName);
-    photo.status = 'fulfilled';
-    photo.value = upload;
-  } catch (error) {
-    photo.status = 'rejected';
-    photo.value = error.toString();
+    promise2.status = 'fulfilled';
+    promise2.value = upload;
+  } catch (err) {
+    promise2.status = 'rejected';
+    promise2.value = err.toString();
   }
-  return [user, photo];
+
+  return [promise1, promise2];
 }
+
+export default handleProfileSignup;
