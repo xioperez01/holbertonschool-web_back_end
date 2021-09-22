e3080cf5af5788675f76265b1aa632e4efb3a309
xioperez01 <angie.xioperez@gmail.com>
2021-09-21 20:17:47 -0500

    Initial code Task 10

---
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,7 @@
+function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw Error('Cannot process');
+  for (const i of map) if (i[1] === 1) map.set(i[0], 100);
+  return map;
+}
+
+export default updateUniqueItems;
