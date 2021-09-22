5b6c22ee12d107aef4207abd13f8a5b8c363eea4
xioperez01 <angie.xioperez@gmail.com>
2021-09-21 20:02:56 -0500

    Initial code Task 5

---
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,15 @@
+function createInt8TypedArray(length, position, value) {
+  if (position >= length) throw Error('Position outside range');
+
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer, 0, 10);
+
+  try {
+    view.setInt8(position, value);
+    return view;
+  } catch (e) {
+    throw Error('Position outside range');
+  }
+}
+
+export default createInt8TypedArray;
