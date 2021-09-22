9bd8f69eca0da4634a03028f0a494c9c0129e827
xioperez01 <angie.xioperez@gmail.com>
2021-09-21 20:23:45 -0500

    Initial code Advanced Task

---
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,11 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (weakMap.has(endpoint)) {
+    const queriesCount = weakMap.get(endpoint);
+    if (queriesCount >= 5) throw Error('Endpoint load is high');
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+}
