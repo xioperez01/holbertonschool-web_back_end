70cec17812a1d6311eff2b4df738fc0db717c2f8
xioperez01 <angie.xioperez@gmail.com>
2021-09-29 16:07:06 -0500

    Task 2 Initial code

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -1,47 +1,51 @@
-const calculateNumber = require("./2-calcul_chai.js");
-const { expect } = require("chai");
+const calculateNumber = require("./1-calcul");
+const assert = require("assert");
 
 describe("calculateNumber - SUM", function () {
   it("output with positive numbers", function () {
-    expect(calculateNumber("SUM", 1, 3)).to.equal(4);
-    expect(calculateNumber("SUM", 1, 3.7)).to.equal(5);
+    assert.strictEqual(calculateNumber("SUM", 1, 3), 4);
+    assert.strictEqual(calculateNumber("SUM", 1, 3.7), 5);
   });
   it("output with negative numbers", function () {
-    expect(calculateNumber("SUM", 1, -3.4)).to.equal(-2);
-    expect(calculateNumber("SUM", -5, -3.5)).to.equal(-8);
+    assert.strictEqual(calculateNumber("SUM", 1, -3.4), -2);
+    assert.strictEqual(calculateNumber("SUM", -5, -3.5), -8);
   });
   it("check arguments/TypeError", function () {
-    expect(() => calculateNumber("SUM", NaN, 0)).to.throw(TypeError);
+    assert.throws(() => calculateNumber("SUM", NaN, 0), { name: "TypeError" });
   });
 });
 
 describe("calculateNumber - SUBTRACT", function () {
   it("output with positive numbers", function () {
-    expect(calculateNumber("SUBTRACT", 1, 3)).to.equal(-2);
-    expect(calculateNumber("SUBTRACT", 1, 3.7)).to.equal(-3);
+    assert.strictEqual(calculateNumber("SUBTRACT", 1, 3), -2);
+    assert.strictEqual(calculateNumber("SUBTRACT", 1, 3.7), -3);
   });
   it("output with negative numbers", function () {
-    expect(calculateNumber("SUBTRACT", 1, -3.4)).to.equal(4);
-    expect(calculateNumber("SUBTRACT", -5, -3.5)).to.equal(-2);
+    assert.strictEqual(calculateNumber("SUBTRACT", 1, -3.4), 4);
+    assert.strictEqual(calculateNumber("SUBTRACT", -5, -3.5), -2);
   });
   it("check arguments/TypeError", function () {
-    expect(() => calculateNumber("SUBTRACT", NaN, 0)).to.throw(TypeError);
+    assert.throws(() => calculateNumber("SUBTRACT", NaN, 0), {
+      name: "TypeError",
+    });
   });
 });
 
 describe("calculateNumber - DIVIDE", function () {
   it("output with positive numbers", function () {
-    expect(calculateNumber("DIVIDE", 15, 3)).to.equal(5);
-    expect(calculateNumber("DIVIDE", 25.4, 3.7)).to.equal(6.25);
+    assert.strictEqual(calculateNumber("DIVIDE", 15, 3), 5);
+    assert.strictEqual(calculateNumber("DIVIDE", 25.4, 3.7), 6.25);
   });
   it("output with negative numbers", function () {
-    expect(calculateNumber("DIVIDE", 60, -3.4)).to.equal(-20);
-    expect(calculateNumber("DIVIDE", -54, -3.5)).to.equal(18);
+    assert.strictEqual(calculateNumber("DIVIDE", 60, -3.4), -20);
+    assert.strictEqual(calculateNumber("DIVIDE", -54, -3.5), 18);
   });
   it("arguments/TypeError", function () {
-    expect(() => calculateNumber("DIVIDE", 0, NaN)).to.throw(TypeError);
+    assert.throws(() => calculateNumber("DIVIDE", 0, NaN), {
+      name: "TypeError",
+    });
   });
   it("division by 0", function () {
-    expect(calculateNumber("DIVIDE", 45, 0)).to.equal("Error");
+    assert.strictEqual(calculateNumber("DIVIDE", 45, 0), "Error");
   });
 });
