0109a2066e8d548d0e6879b4427b860d9fa431f5
xioperez01 <angie.xioperez@gmail.com>
2021-09-29 16:14:07 -0500

    Task 4 Initial code

--- 0x13-unittests_in_js/4-payment.js
+++ 0x13-unittests_in_js/4-payment.js
@@ -1,18 +1,9 @@
-const sinon = require("sinon");
-const { expect } = require("chai");
 const Utils = require("./utils");
-const sendPaymentRequestToApi = require("./4-payment");
 
-describe("sendPaymentRequestToApi", function () {
-  it("validate the usage of the Utils calculateNumber", function () {
-    const logSpy = sinon.spy(console, "log");
-    const calStub = sinon.stub(Utils, "calculateNumber").returns(10);
-    sendPaymentRequestToApi(100, 20);
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
+  console.log(`The total is: ${result}`);
+  return result;
+};
 
-    expect(calStub.calledWith("SUM", 100, 20)).to.be.true;
-    expect(logSpy.calledOnceWith("The total is: 10"));
-
-    logSpy.restore();
-    calStub.restore();
-  });
-});
+module.exports = sendPaymentRequestToApi;
