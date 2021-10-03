72aab8d0f79b855e2d19eca8f68865341f06d528
xioperez01 <angie.xioperez@gmail.com>
2021-10-03 17:22:38 -0500

    Initial code task 4

---
+++ 0x12-Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+}).listen(port, hostname);
+
+module.exports = app;
