be5ca3fb95d0eaba995642fd1f82cfd418f46c3c
xioperez01 <angie.xioperez@gmail.com>
2021-10-09 10:26:42 -0500

    fix any codes

--- 0x14-queuing_system_in_js/8-job.js
+++ 0x14-queuing_system_in_js/8-job.js
@@ -4,19 +4,17 @@ import createPushNotificationsJobs from './8-job.js';
 const queue = kue.createQueue();
 
 const list = [
-    {
-      phoneNumber: '4153518780',
-      message: 'This is the code 1234 to verify your account'
-    }
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account',
+  },
 ];
 
 before(() => queue.testMode.enter());
 afterEach(() => queue.testMode.clear());
 after(() => queue.testMode.exit());
 
-it('does something cool', () => { 
+it('does something cool', () => {
   createPushNotificationsJobs(list, queue);
   expect(queue.testMode.jobs.type).to.equal(Array);
 });
-© 2021 GitHub, Inc.
-Terms
